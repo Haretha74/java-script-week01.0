5f20b851239430ca456db3ad4838905cb3604c3d
Haretha74 <39003632+Haretha74@users.noreply.github.com>
2018-06-09 23:43:15 +0200

    first try with js

---
+++ homework-week01.js
@@ -0,0 +1,5 @@
+            
+var hyf06 = ['Zeeshan', 'Anas', 'Sajid', 'Anuradha', 'Gary', 'Marco', 'Ehsan', 'Haretha', 'Krishna', 'Mohammad Azizul Huq ', 'Mohammad Mosiur Rahman', 'Samara', 'Vignesh', 'Zoey Zou', 'Mohammad Subhiyeh'];
+console.log(hyf06);
+document.getElementById("demo").innerHTML = hyf06;   
+
